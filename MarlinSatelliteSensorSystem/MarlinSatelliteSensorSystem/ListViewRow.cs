be764a0fb914e0ccfdf9687ec5c57bdda0d065b9
SamB-TAFE <30106121@tafe.wa.edu.au>
2026-02-24 13:18:32 +0800

    Finished the Global Methods
    - Added Load Data Method
    - Added ShowAllData Method (ListView Display)
    - Added Galileo Data Creation

---
+++ MarlinSatelliteSensorSystem/MarlinSatelliteSensorSystem/ListViewRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarlinSatelliteSensorSystem
+{
+    public class ListViewRow
+    {
+        public double ValueA { get; set; }
+        public double ValueB { get; set; }
+    }
+}
--- MarlinSatelliteSensorSystem/MarlinSatelliteSensorSystem/MainWindow.xaml.cs
+++ MarlinSatelliteSensorSystem/MarlinSatelliteSensorSystem/MainWindow.xaml.cs
@@ -16,9 +16,66 @@ namespace MarlinSatelliteSensorSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinkedList<double> sensorA = new LinkedList<double>();
+        LinkedList<double> sensorB = new LinkedList<double>();
+
+        Dictionary<string, double> analyticsA = new Dictionary<string, double>();
+        Dictionary<string, double> analyticsB = new Dictionary<string, double>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void LoadData(double mu, double sigma)
+        {
+            sensorA.Clear();
+            sensorB.Clear();
+
+            Galileo6.ReadData dataMaker = new Galileo6.ReadData();
+            sensorA.AddFirst(dataMaker.SensorA(mu, sigma));
+            sensorB.AddFirst(dataMaker.SensorB(mu, sigma));
+
+            for (int i = 0; i < 399; i++)
+            {
+                sensorA.AddLast(dataMaker.SensorA(mu, sigma));
+                sensorB.AddLast(dataMaker.SensorB(mu, sigma));
+            }
+        }
+
+        public void ShowAllSensorData()
+        {
+            DoubleDataView.Items.Clear();
+
+            var nodeA = sensorA.First;
+            var nodeB = sensorB.First;
+
+            while (nodeA != null && nodeB != null)
+            {
+                DoubleDataView.Items.Add(
+                    new ListViewRow
+                    {
+                        ValueA = nodeA.Value,
+                        ValueB = nodeB.Value
+                    });
+
+                nodeA = nodeA.Next;
+                nodeB = nodeB.Next;
+            }
+        }
+
+
+
+        private void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(MeanInput.Text.ToLower(), out var mean) && double.TryParse(DevInput.Text.ToLower(), out var standardDev))
+            {
+                LoadData(mean, standardDev);
+                ShowAllSensorData();
+            }
+            else
+            {
+                MessageBox.Show("Please ensure that the Mean and Standard Dev inputs are valid");
+            }
         }
     }
 }
